00e0e64ba2e7883f139273ff78b13728aac7b033
Riku Ala-Laurinaho <riku.ala-laurinaho@aalto.fi>
2019-08-06 10:59:19 +0300

    Update signals.py

--- management/signals.py
+++ management/signals.py
@@ -12,12 +12,12 @@ def delete_update_file(sender, instance, **kwargs):
 
 @receiver(pre_save, sender=Type_of_sensor)
 def delete_previous_file(sender, instance, **kwargs):
-    #try:
-    old_file = Type_of_sensor.objects.get(pk=instance.pk).handle_data_function
-    new_file = instance.handle_data_function
-    if not old_file == new_file:
-        os.remove(old_file.path)
-    os.rename(new_file.path, old_file.path)
-    new_file.name = old_file.name
-    #except:
-    print("Update file couldn't be deleted. File:", instance.handle_data_function)
+    try:
+        old_file = Type_of_sensor.objects.get(pk=instance.pk).handle_data_function
+        new_file = instance.handle_data_function
+        if not old_file == new_file:
+            os.remove(old_file.path)
+        os.rename(new_file.path, old_file.path)
+        new_file.name = old_file.name
+    except:
+        print("Update file couldn't be deleted. File:", instance.handle_data_function)
