4e945c20ec269944681e7b4330900c15e1eb1265
Riku Ala-Laurinaho <riku.ala-laurinaho@aalto.fi>
2019-08-06 12:37:25 +0300

    Update signals.py

--- management/signals.py
+++ management/signals.py
@@ -19,9 +19,8 @@ def delete_previous_file(sender, instance, **kwargs):
     old_file = tos.handle_data_function
     new_file = instance.handle_data_function
     old_file_name = old_file.name
-    new_file_name = new_file.name
     if old_file == new_file:
         os.rename(new_file.path, old_file.path)
-        new_file.name = old_file.name
+        new_file.name = old_file_name
     os.remove(old_file.path)
     #print("Update file couldn't be deleted. File:", instance.handle_data_function)
