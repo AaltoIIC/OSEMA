80c4f8a81b5f65d52d21f10e35887150429559b7
Riku Ala-Laurinaho <riku.ala-laurinaho@aalto.fi>
2019-08-06 12:42:20 +0300

    Update signals.py

--- management/signals.py
+++ management/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
-from management.models import Update, Type_of_sensor, DoesNotExist
+from management.models import Update, Type_of_sensor
 import os
 
 @receiver(pre_delete, sender=Update)
