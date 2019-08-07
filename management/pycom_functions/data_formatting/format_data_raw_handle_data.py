83438f022776a6a26eafbe6e9bf51e14b7670e1d
Riku Ala-Laurinaho <riku.ala-laurinaho@aalto.fi>
2019-08-07 10:04:07 +0300

    Update format_data_raw_handle_data.py

--- management/pycom_functions/data_formatting/format_data_raw_handle_data.py
+++ management/pycom_functions/data_formatting/format_data_raw_handle_data.py
@@ -7,5 +7,5 @@ def format_data(header_ts, data):
     data_string += "Data:"
     for value_pair in data:
         ts_packed = ustruct.pack("<L", value_pair[1])
-        data_string += handle_data(value_pair[0]) + ts_packed
+        data_string += str(handle_data(value_pair[0]) + ts_packed)
     return data_string
