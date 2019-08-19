29460d59a91ce66a090074ce8a7dd3011cab6fdb
Riku Ala-Laurinaho <alalar1@aalto.fi>
2019-08-19 16:08:19 +0300

    fixed error with & operand

--- management/pycom_functions/read_values_garmin_lidar_lite_v3HP.py
+++ management/pycom_functions/read_values_garmin_lidar_lite_v3HP.py
@@ -1,8 +1,8 @@
 """Read values from sensor"""
 def read_values(i2c):
     i2c.writeto_mem(ADDRESS, 0, 2) #Start measurement
-    busy = 1
-    while (busy & 1) != 0:
+    busy = b"1"
+    while (busy[0] & 1) != 0:
         busy = i2c.readfrom_mem(ADDRESS, 1, 1) #wait until measurement is done
     readed_values = b""
     for key in READ_DICT:
