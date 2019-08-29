4f3ba95deda98f5d700e12b39125b1f24019320f
Riku Ala-Laurinaho <alalar1@aalto.fi>
2019-08-29 14:04:29 +0300

    Added maximum time to connect before resetting board

--- management/pycom_functions/connect_network_wlan.py
+++ management/pycom_functions/connect_network_wlan.py
@@ -5,6 +5,8 @@ def connect_network(flash_light=True):
     ssid, auth, identity = NETWORK_SETTINGS
     wlan = WLAN(mode=WLAN.STA)
     wlan.connect(ssid=ssid, auth=auth, identity=identity)
+    counter = 1
+    counter_max = 20 #trying to connect max 20 seconds
     while not wlan.isconnected():
         if flash_light:
             pycom.rgbled(0xCD7300) #Orange
@@ -12,4 +14,8 @@ def connect_network(flash_light=True):
         if flash_light:
             pycom.rgbled(0x00000)  # Black
         utime.sleep(0.5)
+        counter += 1
+        if counter == counter_max:
+            print("Couldn't connect to network: ", ssid, " Resetting board!")
+            machine.reset()
     return wlan
