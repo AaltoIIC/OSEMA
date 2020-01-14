5ef6b3f5d46fbaf8b12e626020482279fa9cbf67
AIIC <AIIC@aalto.fi>
2020-01-14 15:46:27 +0200

    modified hmac library

--- management/pycom_functions/hmac.py
+++ management/pycom_functions/hmac.py
@@ -1,11 +1,9 @@
-#From https://github.com/micropython/micropython-lib/blob/master/hmac/hmac.py
+#from: https://forum.pycom.io/topic/2180/hmac-on-on-lopy/3
 """HMAC (Keyed-Hashing for Message Authentication) Python module.
-
 Implements the HMAC algorithm as described by RFC 2104.
 """
+
 import hashlib as _hashlib
-PendingDeprecationWarning = None
-RuntimeWarning = None
 
 trans_5C = bytes((x ^ 0x5C) for x in range(256))
 trans_36 = bytes((x ^ 0x36) for x in range(256))
@@ -21,14 +19,12 @@ digest_size = None
 
 class HMAC:
     """RFC 2104 HMAC class.  Also complies with RFC 4231.
-
     This supports the API for Cryptographic Hash Functions (PEP 247).
     """
     blocksize = 64  # 512-bit HMAC; can be changed in subclasses.
 
     def __init__(self, key, msg = None, digestmod = None):
         """Create a new HMAC object.
-
         key:       key for the keyed hash object.
         msg:       Initial input for the hash, if provided.
         digestmod: A module supporting PEP 247.  *OR*
@@ -37,7 +33,6 @@ class HMAC:
                    Defaults to hashlib.md5.
                    Implicit default to hashlib.md5 is deprecated and will be
                    removed in Python 3.6.
-
         Note: key and msg must be a bytes or bytearray objects.
         """
 
@@ -45,8 +40,8 @@ class HMAC:
             raise TypeError("key: expected bytes or bytearray, but got %r" % type(key).__name__)
 
         if digestmod is None:
-            print("warning: no digest mode given")
-            digestmod = _hashlib.md5
+            raise Exception("HMAC() without an explicit digestmod argument "
+                           "is deprecated.")
 
         if callable(digestmod):
             self.digest_cons = digestmod
@@ -55,29 +50,16 @@ class HMAC:
         else:
             self.digest_cons = lambda d=b'': digestmod.new(d)
 
-        self.outer = self.digest_cons()
         self.inner = self.digest_cons()
-        self.digest_size = self.inner.digest_size
+        self.digest_size = 32
+        self.block_size = 64
 
-        if hasattr(self.inner, 'block_size'):
-            blocksize = self.inner.block_size
-            if blocksize < 16:
-                print('block_size seems too small; using our')
-                blocksize = self.blocksize
-        else:
-            print('No block_size attribute on given digest object')
-            blocksize = self.blocksize
+        if len(key) > self.block_size:
+            raise Exception('key too long')
 
-        # self.blocksize is the default blocksize. self.block_size is
-        # effective block size as well as the public API attribute.
-        self.block_size = blocksize
-
-        if len(key) > blocksize:
-            key = self.digest_cons(key).digest()
-
-        key = key + bytes(blocksize - len(key))
-        self.outer.update(translate(key, trans_5C))
+        key = key + bytes(self.block_size - len(key))
         self.inner.update(translate(key, trans_36))
+        self.outer_key_translated = translate(key, trans_5C)
         if msg is not None:
             self.update(msg)
 
@@ -90,51 +72,25 @@ class HMAC:
         """
         self.inner.update(msg)
 
-    def copy(self):
-        """Return a separate copy of this hashing object.
-
-        An update to this copy won't affect the original object.
-        """
-        # Call __new__ directly to avoid the expensive __init__.
-        other = self.__class__.__new__(self.__class__)
-        other.digest_cons = self.digest_cons
-        other.digest_size = self.digest_size
-        other.inner = self.inner.copy()
-        other.outer = self.outer.copy()
-        return other
-
-    def _current(self):
-        """Return a hash object for the current state.
-
-        To be used only internally with digest() and hexdigest().
-        """
-        h = self.outer.copy()
-        h.update(self.inner.digest())
-        return h
 
     def digest(self):
         """Return the hash value of this hashing object.
-
         This returns a string containing 8-bit data.  The object is
         not altered in any way by this function; you can continue
         updating the object after calling this function.
         """
-        h = self._current()
-        return h.digest()
+        inner_digest = self.inner.digest()
+        self.outer = self.digest_cons()
+        self.outer.update(self.outer_key_translated)
+        self.outer.update(inner_digest)
+        return self.outer.digest()
 
-    def hexdigest(self):
-        """Like digest(), but returns a string of hexadecimal digits instead.
-        """
-        h = self._current()
-        return h.hexdigest()
 
 def new(key, msg = None, digestmod = None):
     """Create a new hashing object and return it.
-
     key: The starting key for the hash.
     msg: if available, will immediately be hashed into the object's starting
     state.
-
     You can now feed arbitrary strings into the object using its update()
     method, and can ask for the hash value at any time by calling its digest()
     method.
