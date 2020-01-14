aae0145fdfd1e47c5a122a8a225cd8c6aca4cc48
AIIC <AIIC@aalto.fi>
2020-01-14 15:15:31 +0200

    fixed encrypt and decrypt functions

--- management/pycom_functions/in_every_program/decrypt_msg.py
+++ management/pycom_functions/in_every_program/decrypt_msg.py
@@ -2,10 +2,10 @@
 def decrypt_msg(msg, key): #msg = byte-string, key = 128-bit key as a byte-string
     n = 16
     encrypted_data_list = [msg[i:i+n] for i in range(0, len(msg), n)]
-    cipher = AES(shared_secret, AES.MODE_CBC, encrypted_data_list[0])
+    cipher = AES(key, AES.MODE_CBC, encrypted_data_list[0])
     msg = ""
     for block in encrypted_data_list[1:]:
     	decrypted_msg = cipher.decrypt(block)
     	msg += decrypted_msg.decode("ascii")
-    	cipher = AES(shared_secret, AES.MODE_CBC, block)
+    	cipher = AES(key, AES.MODE_CBC, block)
     return msg
--- management/pycom_functions/in_every_program/encrypt_msg.py
+++ management/pycom_functions/in_every_program/encrypt_msg.py
@@ -6,13 +6,13 @@ def encrypt_msg(msg, key): #msg = string, key = 128-bit key as a byte-string
     padded_content_list = [padded_content[i:i+n] for i in range(0, len(padded_content), n)] #divide into list
 
     iv = crypto.getrandbits(128) # hardware generated random IV (never reuse it)
-    cipher = AES(shared_secret, AES.MODE_CBC, iv)
+    cipher = AES(key, AES.MODE_CBC, iv)
     encrypted_string = iv
 
     #encryption
     for block in padded_content_list:
     	ciphertext = cipher.encrypt(block.encode("ascii"))
     	encrypted_string += ciphertext
-    	cipher = AES(shared_secret, AES.MODE_CBC, ciphertext)
+    	cipher = AES(key, AES.MODE_CBC, ciphertext)
 
     return ubinascii.hexlify(encrypted_string).decode("ascii")
