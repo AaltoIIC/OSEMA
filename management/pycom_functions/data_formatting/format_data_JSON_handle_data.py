b43165a3c218ec5988908c344b2bd4b2c74846a6
Riku Ala-Laurinaho <alalar1@aalto.fi>
2020-01-17 14:01:13 +0200

    corrected json

--- management/pycom_functions/data_formatting/format_data_JSON_handle_data.py
+++ management/pycom_functions/data_formatting/format_data_JSON_handle_data.py
@@ -1,20 +1,20 @@
 def format_data(header_ts, data):
     header_ts_epoch = convert_to_epoch(header_ts)
-    data_string = "{\n"
-    data_string += "\t'Begin':{},\n".format(header_ts_epoch)
-    data_string += "\t'Data': [\n"
+    data_string = '{\n'
+    data_string += '\t"Begin":{},\n'.format(header_ts_epoch)
+    data_string += '\t"Data": [\n'
     for value_pair in data:
-        data_string += "\t\t {"
+        data_string += '\t\t {'
         value_pair[0] = handle_data(value_pair[0])
         data_values = ustruct.unpack(FORMAT_STRING[:-1], value_pair[0])
         max = len(VARIABLE_NAMES)
         for i in range(len(VARIABLE_NAMES) + 1):
             if i == max:
-                data_string += "'time':{}".format(header_ts_epoch + value_pair[1])
+                data_string += '"time":{}'.format(header_ts_epoch + value_pair[1])
             else:
-                data_string += "'{}':{}, ".format(VARIABLE_NAMES[i], data_values[i])
-        data_string += "},\n"
+                data_string += '"{}":{}, '.format(VARIABLE_NAMES[i], data_values[i])
+        data_string += '},\n'
     data_string = data_string[:-1]
-    data_string += "\t]\n"
-    data_string += "}\n"
+    data_string += '\t]\n'
+    data_string += '}\n'
     return data_string
